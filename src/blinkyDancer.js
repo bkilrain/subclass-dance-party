f51d2db1d78c699bdb8a99758456741fc52afbf2
bkilrain <bkilrain@gmail.com>
2016-08-01 19:26:52 -0700

    Implement pseudoclassical refactoring... blinking rate test not passed

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,28 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
+var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
+  MakeDancer.call(this, top, left, timeBetweenSteps);
+  
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
+  
+  this.timeBetweenSteps = timeBetweenSteps;
+ 
+  
+};
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
+MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
+MakeBlinkyDancer.prototype.constructor = MakeBlinkyDancer;
 
-  return blinkyDancer;
+//MakeBlinkyDancer.prototype.oldStep = MakeDancer.prototype.step.bind(this);
+
+MakeBlinkyDancer.prototype.step = function() {
+  var oldStep = MakeDancer.prototype.step.bind(this);
+  // call the old version of step at the beginning of any call to this new version of step
+  //setTimeout(this.step, timeBetweenSteps);
+  oldStep();
+  
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
 };
